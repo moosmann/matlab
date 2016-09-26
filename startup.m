6d1c600a67e329486e12e9ff294ac6e852bc46b2
Julian Moosmann <julian.moosmann@gmail.com>
2016-09-26 18:15:07 +0200

    Move startup.m to main matlab folder.

---
+++ startup.m
@@ -0,0 +1,19 @@
+%% Scripts and functions
+addpath( genpath( [getenv('HOME') '/matlab'] ) );
+rmpath( genpath(  [getenv('HOME') '/matlab/.git'] ) );
+rmpath( genpath(  [getenv('HOME') '/matlab/test'] ) );
+rmpath( genpath(  [getenv('HOME') '/matlab/old'] ) );
+
+%% Set default color map to grayscale instead of jet
+set(groot,'DefaultFigureColormap',gray)
+close all;
+
+%% BUG FIXES
+
+% Shall fix error: An unexpected error occurred during CUDA execution. The
+% CUDA error was: cannot set while device is active in this process.
+%a = gpuArray(1); 
+%clear a;
+
+% Fix Matlab bug: “dlopen: cannot load any more object with static TLS”
+%ones(10) * ones(10);
