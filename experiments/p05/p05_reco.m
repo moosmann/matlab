acc4778d3e55342a71660554171c5eaf845812a9
Julian Moosmann <julian.moosmann@gmail.com>
2017-02-15 17:54:33 +0100

    Fix paramter loop

--- experiments/p05/p05_reco.m
+++ experiments/p05/p05_reco.m
@@ -174,7 +174,7 @@ if exist( 'external_parameter' ,'var')
     for nn = 1:numel(fields)
         var_name = fields{nn};
         var_val = getfield(external_parameter, var_name );
-        assignin('base', var_name, var_val )
+        assignin('caller', var_name, var_val )
     end
     clear external_parameter;
 end
