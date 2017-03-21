6c54fe3380f71aeec6ffd2704fab316e51ab9b53
Julian Moosmann <julian.moosmann@gmail.com>
2017-03-21 16:56:49 +0100

    Add path to startup depending on host

--- startup.m
+++ startup.m
@@ -6,14 +6,23 @@ rmpath( genpath(  [userpath '/.git'] ) );
 %rmpath( genpath(  [userpath '/test'] ) );
 rmpath( genpath(  [userpath '/old'] ) );
 
-fprintf( 'HOSTNAME: %s\n', getenv('HOSTNAME') );
+hostname = getenv('HOSTNAME');
+fprintf( 'HOSTNAME: %s\n', hostname );
 fprintf( 'userpath: %s', userpath );
 
-%% ASTRA 
-% ASTRA 1.6
-%addpath( genpath( '/usr/share/astra/matlab' ) );
-% ASTRA 1.7.1
-addpath( genpath( '/opt/xray/astra-toolbox/1.7.1/matlab' ) );
+%% ASTRA
+% ASTRA 1.8 local
+path_to_astra = '/asap3/petra3/gpfs/common/p05/astra/1.8/matlab';
+% ASTRA 1.7.1 global, probably broken
+%path_to_astra = '/opt/xray/astra-toolbox/1.7.1/matlab';
+if strcmp( hostname(1:8), 'max-hzgg')    
+    addpath( genpath( path_to_astra ) );
+elseif strcmp( hostname(1:8), 'max-p3ag')   
+    addpath( genpath( path_to_astra ) );
+else    
+    addpath( genpath( path_to_astra ) );
+    %addpath( genpath( '/usr/share/astra/matlab' ) );
+end
 
 %% Set default color map to grayscale instead of jet
 set(groot, 'DefaultFigureColormap', gray)
