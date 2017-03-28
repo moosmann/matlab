84a04770d0fbdbc8de48b3d8d9cf46b2ac3a7d8a
Julian Moosmann <julian.moosmann@gmail.com>
2017-03-28 11:54:58 +0200

    Correlation 2nd try

--- utilities/ImageCorrelation2.m
+++ utilities/ImageCorrelation2.m
@@ -5,16 +5,14 @@ function [out, corr_map] = ImageCorrelation2(im1, im2, padding, use_gpu, verbose
 %
 % Written Julian Moosamnn. Last modification: 2017-02-20, GPU support added
 
-%% Try interpolation using zero padding in fourier space
-
 %% Default arguments %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if nargin < 1
     im1 = zeros(8);
-    im1(4,4) = 1;
+    im1(4,3:5) = [1 3 2];
 end
 if nargin < 2
     im2 = zeros(8);
-    im2(4,4:5) = 0.5;
+    im2(4,4:6) = [2 5 1];
 end
 if nargin < 3
     padding = 0;
@@ -29,8 +27,8 @@ end
 %% Main %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Padding
-im1 = padarray( im1, padding * size( im1 ), 0, 'both');
-im2 = padarray( im2, padding * size( im2 ), 0, 'both');
+im1 = padarray( im1, padding * size( im1 ), 0, 'post');
+im2 = padarray( im2, padding * size( im2 ), 0, 'post');
 
 % Normalize
 im1 = SubtractMean( im1 ) / std2( im1 );
@@ -47,16 +45,28 @@ if use_gpu
     im2 = gpuArray( im2 );
 end
 
+res = 1;
+
 % Cross-correlation map
-corr_map = fftshift( ifft2( padarray( fft2( im1 ) .* fft2( rot90(im2, 2) ), 0, 'post'), 'symmetric' ) );
+corr_map = fft2( im1 ) .* fft2( rot90(im2, 2) );
+corr_map = padarray( corr_map, (res - 1) * size(corr_map), 0, 'post');
+corr_map = fftshift( ifft2( corr_map, 'symmetric' ) );
+%whos corr_map
 
 % Correlation map maximum
-[val, pos] = max( corr_map(:) );
+val = max( corr_map(:) );
+[m2, m1] = meshgrid( 1:d2, 1:d1);
+corr_map = corr_map == val;
+N = sum( corr_map(:));
+pos1 = corr_map .* m1;
+pos1 = sum( pos1(:) ) / N;
+pos2 = corr_map .* m2;
+pos2 = sum( pos2(:) ) / N;
+fprintf( '\n pos1 : %g \n pos2 : %g \n', pos1, pos2 )
 
-% Linear indexing to subscript indexing (row, column)
-[pos1, pos2] = ind2sub( [d1 d2], pos);
-pos1 = - pos1 + d1 / 2;
-pos2 = - pos2 + d2 / 2;
+%fprintf( '\n\n pos2 : %g \n\n', pos2)
+pos1 = - pos1 + res * d1 / 2;
+pos2 = - pos2 + res * d2 / 2;
 
 %% Check
 % Add 0.5 or 1 pixel to peak position for even or odd dimensions
