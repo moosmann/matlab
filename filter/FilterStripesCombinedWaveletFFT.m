00d153e447f9d735484840a2cde18ff8ded8f079
Julian Moosmann <julian.moosmann@gmail.com>
2017-06-02 16:22:57 +0200

    Crop back to original shape. Add doc on available wavelet types.

--- filter/FilterStripesCombinedWaveletFFT.m
+++ filter/FilterStripesCombinedWaveletFFT.m
@@ -12,7 +12,7 @@ function [im_filt] = FilterStripesCombinedWaveletFFT(im, dec_levels, wname, sigm
 % PARAMETER:
 % im : 2D input image to be stripe filtered
 % dec_levels : 1D-vector of integers in [1, max_level]. decompositon level 
-% wname : string, wavelet type. default: 'db25'
+% wname : string, wavelet type. default: 'db25'. 'bior2.8'
 % sigma : scalar or 1D-vector with numel(sigma)=max(dec_levels), damping
 % factor, if scalar same sigma is used for all levels.
 %
@@ -23,6 +23,33 @@ function [im_filt] = FilterStripesCombinedWaveletFFT(im, dec_levels, wname, sigm
 % Modified by Julian Moosmann, 2017-05-30.
 %
 % [im_filt] = FilterStripesCombinedWaveletFFT(im, dec_levels, wname, sigma)
+
+% Daubechies
+% 	'db1' or 'haar', 'db2', ... ,'db10', ... , 'db45'
+% 
+% Coiflets
+% 	'coif1', ... , 'coif5'
+% 
+% Symlets
+% 	'sym2', ... , 'sym8', ... ,'sym45'
+% 
+% Fejer-Korovkin filters
+% 	'fk4', 'fk6', 'fk8', 'fk14', 'fk22'
+% 
+% Discrete Meyer
+% 	'dmey'
+% 
+% Biorthogonal
+% 	'bior1.1', 'bior1.3', 'bior1.5'
+% 'bior2.2', 'bior2.4', 'bior2.6', 'bior2.8'
+% 'bior3.1', 'bior3.3', 'bior3.5', 'bior3.7'
+% 'bior3.9', 'bior4.4', 'bior5.5', 'bior6.8'
+% 
+% Reverse Biorthogonal
+% 	'rbio1.1', 'rbio1.3', 'rbio1.5'
+% 'rbio2.2', 'rbio2.4', 'rbio2.6', 'rbio2.8'
+% 'rbio3.1', 'rbio3.3', 'rbio3.5', 'rbio3.7'
+% 'rbio3.9', 'rbio4.4', 'rbio5.5', 'rbio6.8'
 
 %% TODO: add direction option
 
@@ -43,6 +70,7 @@ end
 
 %% Main %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+[d1, d2] = size( im );
 dec_num = max( dec_levels );
 
 % wavelet decomposition
@@ -50,7 +78,7 @@ Ch{dec_num} = [];
 Cv{dec_num} = [];
 Cd{dec_num} = [];
 for ii = 1:dec_num
-    [im, Ch{ii}, Cv{ii}, Cd{ii}] = dwt2(im, wname);
+    [im, Ch{ii}, Cv{ii}, Cd{ii}] = dwt2( im, wname);    
 end
 
 % FFT transform of horizontal frequency bands
@@ -61,6 +89,7 @@ for ii = dec_levels
     
     % FFT
     fCv = fftshift( fft( Cv{ii} ) );
+    
     [my, mx] = size( fCv );
     
     % damping of vertical stripe information
@@ -69,6 +98,7 @@ for ii = dec_levels
     
     % inverse FFT
     Cv{ii} = ifft( ifftshift( fCv ) );
+
 end
 
 % wavelet reconstruction
@@ -77,5 +107,7 @@ for ii = dec_num:-1:1
     im_filt = im_filt(1:size(Ch{ii},1),1:size(Ch{ii},2));
     im_filt = idwt2( im_filt, Ch{ii}, Cv{ii}, Cd{ii}, wname);
 end
+
+im_filt = im_filt(1:d1,1:d2);
 
 return
