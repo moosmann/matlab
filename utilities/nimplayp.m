a5839dc05053dda103665e6f3dcdd547b82a6c91
Julian Moosmann <julian.moosmann@gmail.com>
2018-04-30 19:03:08 +0200

    Fix default

--- utilities/nimplayp.m
+++ utilities/nimplayp.m
@@ -1,11 +1,11 @@
-function nimplayp(imstack,normGlobal,permuteOrder)
+function nimplayp(imstack,renorm_slicewise,permuteOrder)
 % Call nimplay with predefined permuation order. See function nimplay for
 % details.
 %
 % Written by Julian Moosmann, last version 2013-10-18, 2017-04-27
 
 if nargin < 2
-    normGlobal = 0;
+    renorm_slicewise = 1;
 end
 if nargin < 3
     permuteOrder = [2 1 3];
@@ -13,4 +13,4 @@ end
 
 fprintf('Rearrange array dimensions by permutation vector %s\n',mat2str(permuteOrder));
 %% Call nimplay
-nimplay(imstack,normGlobal,permuteOrder);
+nimplay(imstack,renorm_slicewise,permuteOrder);
